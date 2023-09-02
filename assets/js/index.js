b5296e39795b5f0a6d2f3bf79df374411d910f38
TharakaUJ <9dmpires2k17.tuj@gmail.com>
2023-09-02 17:14:27 +0530

    three js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,7 @@
+---
+
+---
+
 const verticalGrids = document.getElementsByClassName('vertical-grid');
 
 	const speeds = [0.1, 0.22, 0.14];
@@ -137,14 +141,110 @@ function section2Animation() {
 		clearInterval(intervalScroll2);
 		clearInterval(intervalScroll3);
 		main.remove();
+		animate();
+
 	}, 3000);
 
 	sectionAnimHaventDone = false;
 }
 
 
+
+
+
+
+//THREE js part
+
+import * as THREE from 'three';
+
+
+const canvasContainer = document.getElementById('canvas-container')
+
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const renderer = new THREE.WebGLRenderer({ preserveDrawingBuffer: false, alpha: true })
+  renderer.sortObjects = false
+  renderer.autoClearColor = false
+
+  //Camera initialization
+  camera.position.z = 55
+
+  //Add the background
+  renderer.setClearColor("#000", 1);
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setPixelRatio(window.devicePixelRatio)
+
+  //Add the scene
+  canvasContainer.appendChild(renderer.domElement)
+
+  let imgs = []
+
+
+  addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+  })
+
+
+  
+// instantiate a loader
+const loader = new THREE.TextureLoader();
+
+
+
+{% assign image_files = site.static_files | where: "image", true %}
+const picNames = [{% for image in image_files %}"{{ image.path }}",{% endfor %}]
+//const picNames = ["/pics/GPIO-pinouts.jpg", "/pics/featured-image-for-desktop-without-monitor-with-VNC.png", "/pics/steel-mountain-hack-mr.-robot-e1686160421682-1080x675.png", "/pics/Screenshot-from-2023-04-26-16.33.55@2x-e1682518769168.png", "/pics/save-the-code.png", "/pics/recomended-software.png"]
+
+
+for (let i =0; i < picNames.length; i++) {
+    let picName = picNames[i];
+    let url = "{{ site.baseurl }}" + picName;
+    let planegeometry = new THREE.PlaneGeometry(10,10);
+    let planematerial = new THREE.MeshLambertMaterial({
+        color: 0xffffff,
+        map: loader.load(url)
+    });
+    let img = new THREE.Mesh(planegeometry, planematerial);
+    img.position.x = Math.random() * 100 - 50;
+    img.position.y = Math.random() * 1000 - 50;
+    img.position.z = Math.random() * 50 - 25;   
+    scene.add(img);
+    imgs.push(img)
+  };
+
+  function animate() {
+    requestAnimationFrame( animate );
+      renderer.render( scene, camera );
+    
+
+    const speed = -0.3
+
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].position.y += speed
+    
+        if (imgs[i].position.y >= 60) {
+        imgs[i].position.x = Math.random() * 100 - 50
+        imgs[i].position.z = Math.random() * 100 - 50
+        imgs[i].position.y = 5
+        }
+      }
+
+    }
+
+
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    directionalLight.position.set(0, 0, 60)
+    scene.add( directionalLight );
+
+
+
+
+
+
 /*
-<script type="module">
 import * as THREE from 'three';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
