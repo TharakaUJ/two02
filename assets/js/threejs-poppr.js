3600d24fe50e64fa555c0fcbc9d5d9789036a141
TharakaUJ <9dmpires2k17.tuj@gmail.com>
2023-10-09 18:56:56 +0530

    working with two bugs

--- assets/js/threejs-poppr.js
+++ assets/js/threejs-poppr.js
@@ -1,22 +1,26 @@
+---
+---
+
 import * as THREE from "https://esm.sh/three";
 
 var mouse = { x: 0, y: 0 };
 var planePos = { x: 0, y: 0, z: 0 };
+var planes = [];
 
 const container = document.getElementById("insights");
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 );
 
 camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer({
-  alpha: true
+    alpha: true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 container.appendChild(renderer.domElement);
@@ -26,98 +30,104 @@ directionalLight.position.set(0, 0, 60);
 scene.add(directionalLight);
 
 const loader = new THREE.TextureLoader();
-const imag = "https://images.unsplash.com/photo-1695512294611-80be329e683b?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2OTY3NjQ4NjN8&ixlib=rb-4.0.3&q=85";
+const images = ["https://images.unsplash.com/photo-1695512294611-80be329e683b?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2OTY3NjQ4NjN8&ixlib=rb-4.0.3&q=85", "{{ site.baseurl }}/pics/featured-image-for-desktop-without-monitor-with-VNC.png", "{{ site.baseurl }}/pics/finding-ip-adress-featured-img.png", "{{ site.baseurl }}/pics/finding-ip-adress-featured-img.png"];
 
 function createAPlane(imag) {
-  const geometry = new THREE.PlaneGeometry(1.5, 2);
-  const material = new THREE.MeshLambertMaterial({
-    color: 0xffffff,
-    map: loader.load(imag),
-    opacity: 0,
-    transparent: true
-  });
-  const object = new THREE.Mesh(geometry, material);
-  return object;
+    const geometry = new THREE.PlaneGeometry(1.5, 2);
+    const material = new THREE.MeshLambertMaterial({
+        color: 0xffffff,
+        map: loader.load(imag),
+        opacity: 0,
+        transparent: true
+    });
+    const object = new THREE.Mesh(geometry, material);
+    return object;
 }
 
-const plane = createAPlane(imag);
+//const plane = createAPlane(imag);
 
 container.addEventListener("mousemove", (event) => {
-  planePos = onMouseMove(event);
+    planePos = onMouseMove(event);
 });
 
 const hoverElements = document.getElementsByClassName("insight-items");
 
-[...hoverElements].forEach((hoverElement) => {
-  hoverElement.addEventListener("mouseenter", () => {
-    atHoverStart(plane, hoverElement);
-    plane.material.opacity = 1;
-  });
+for (let i = 0; i < hoverElements.length; i++) {
+    //[...hoverElements].forEach((hoverElement) => {
+    planes.push(createAPlane(images[i]));
+    hoverElements[i].addEventListener("mouseenter", () => {
+        atHoverStart(planes[i], hoverElements[i]);
+        planes[i].material.opacity = 1;
+    });
 
-  hoverElement.addEventListener("mouseleave", () => {
-    atHoverEnd(plane.material, plane);
-  });
-});
+    hoverElements[i].addEventListener("mouseleave", () => {
+        atHoverEnd(planes[i].material, planes[i]);
+    });
+    //});
+}
 
 // Follows the mouse event
 function onMouseMove(event) {
-  // Update the mouse variable
-  event.preventDefault();
+    // Update the mouse variable
+    event.preventDefault();
 
-  //dafault * 2 -1
-  mouse.x = (event.clientX / window.innerWidth) * 1.8 - 0.8;
+    //dafault * 2 -1
+    mouse.x = (event.clientX / window.innerWidth) * 1.8 - 0.8;
 
-  //default *2 + 1
-  mouse.y = -(event.clientY / window.innerHeight) * 1.8 + 1;
+    //default *2 + 1
+    mouse.y = -(event.clientY / window.innerHeight) * 1.8 + 1;
 
-  var pos = IntoThreeD(mouse.x, mouse.y);
-  //plane.position.copy(pos);
+    var pos = IntoThreeD(mouse.x, mouse.y);
+    //plane.position.copy(pos);
 
-  return pos;
+    return pos;
 
-  // Make the sphere follow the mouse
-  //	mouseMesh.position.set(event.clientX, event.clientY, 0);
+    // Make the sphere follow the mouse
+    //	mouseMesh.position.set(event.clientX, event.clientY, 0);
 }
 
 function IntoThreeD(x, y) {
-  // Make the object follow the mouse
-  var vector = new THREE.Vector3(x, y, 0.5);
-  vector.unproject(camera);
-  var dir = vector.sub(camera.position).normalize();
-  var distance = -camera.position.z / dir.z;
-  var pos = camera.position.clone().add(dir.multiplyScalar(distance));
-  return pos;
+    // Make the object follow the mouse
+    var vector = new THREE.Vector3(x, y, 0.5);
+    vector.unproject(camera);
+    var dir = vector.sub(camera.position).normalize();
+    var distance = -camera.position.z / dir.z;
+    var pos = camera.position.clone().add(dir.multiplyScalar(distance));
+    return pos;
 }
 
 function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-  plane.position.lerp(planePos, 0.02);
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    //plane.position.lerp(planePos, 0.02);
+    [...planes].forEach((plane) => {
+        plane.position.lerp(planePos, 0.02);
+    });
 }
 animate();
 
 var hoverEndRAFId;
 
 function atHoverEnd(material, object) {
-  if (material.opacity > 0) {
-    hoverEndRAFId = requestAnimationFrame(() => {
-      atHoverEnd(material, object);
-    });
-    material.opacity = material.opacity - 0.025;
-    console.log(material.opacity);
-  } else {
-    scene.remove(object);
-  }
+    if (material.opacity > -4) {
+        hoverEndRAFId = requestAnimationFrame(() => {
+            atHoverEnd(material, object);
+        });
+        material.opacity = material.opacity - 0.05;
+    } else {
+        scene.remove(object);
+    }
 }
 
 function atHoverStart(object, element) {
-  if (object.parent != scene) {
-    scene.add(object);
-    let elRect = element.getBoundingClientRect();
-    let elStartX = (elRect.right / window.innerWidth) * 2 - 1;
-    let elStartY = -(elRect.top / window.innerWidth) * 2 + 1;
-    plane.position.copy(IntoThreeD(elStartX, elStartY));
-  } else {
-    cancelAnimationFrame(hoverEndRAFId);
-  }
+    if (object.parent != scene) {
+        scene.add(object);
+        let elRect = element.getBoundingClientRect();
+        let elStartX = (elRect.right / window.innerWidth) * 2 - 1;
+        let elStartY = -(elRect.top / window.innerWidth) * 2 + 1;
+        object.position.copy(IntoThreeD(elStartX, elStartY));
+        planes.indexOf(object);
+    } else {
+        cancelAnimationFrame(hoverEndRAFId);
+    }
 }
