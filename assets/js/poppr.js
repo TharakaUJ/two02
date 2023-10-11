7f17302545a9c55de756e29352bd4ec9eda1d23d
TharakaUJ <9dmpires2k17.tuj@gmail.com>
2023-10-11 17:19:18 +0530

    function trotling for scroll events

--- assets/js/poppr.js
+++ assets/js/poppr.js
@@ -1,13 +1,35 @@
 const root = document.querySelector(':root');
 const scrollBar = document.getElementById('scroll-bar');
+const gap = window.innerWidth * 0.08 //css grid gap in gallery-container
+var initialFact = 0; //horizontal scroll when vertical scroll
 
 root.style.setProperty('--scroll-bar-height', `${window.innerHeight * (window.innerHeight / document.body.scrollHeight)}px`);
 
 window.addEventListener('scroll', () => {
-    root.style.setProperty('--scroll-postion', `${window.innerHeight * window.scrollY / document.body.scrollHeight}px`);
-    scrollBar.animate({
-        opacity: '1'
-    }, { fill: "forwards", duration: 800 })
+    let ticking = false;
+    if (!ticking) {
+        // event throtteling
+        window.requestAnimationFrame(function () {
+            root.style.setProperty('--scroll-postion', `${window.innerHeight * window.scrollY / document.body.scrollHeight}px`);
+            scrollBar.animate({
+                opacity: '1'
+            }, { fill: "forwards", duration: 800 });
+
+            //horizontal scroll when vertical scroll
+            let fact = window.scrollY / document.body.scrollHeight;
+            if (fact <= 0.6) {
+                if (initialFact < fact) {
+                    scrollContainer.scrollLeft = scrollContainer.scrollLeft + fact * 10;
+                } else {
+                    scrollContainer.scrollLeft = scrollContainer.scrollLeft - fact * 10;
+                }
+
+                initialFact = fact
+            }
+            ticking = false;
+        });
+        ticking = true;
+    }
 });
 
 window.addEventListener('scrollend', () => {
@@ -78,25 +100,6 @@ scrollContainer.addEventListener("mousemove", (e) => {
 });
 
 
-
-
-//horizontal scroll when vertical scroll
-var initialFact = 0;
-window.addEventListener('scroll', () => {
-    let fact = window.scrollY / document.body.scrollHeight;
-    if (fact <= 0.6) {
-        if (initialFact < fact) {
-            scrollContainer.scrollLeft = scrollContainer.scrollLeft + fact * 10;
-        } else {
-            scrollContainer.scrollLeft = scrollContainer.scrollLeft - fact * 10;
-        }
-
-        initialFact = fact
-    }
-});
-
-
-
 /* -- Glow effect -- */
 
 const blob = document.getElementById("blob");
@@ -116,29 +119,23 @@ turner.parentNode.onpointermove = event => {
     const { clientX, clientY } = event;
 
     turner.animate({
-        transform: `rotateX(${15-30 * clientY/window.innerHeight}deg) rotateY(${30 * clientX/window.innerWidth - 15}deg)`,
+        transform: `rotateX(${15 - 30 * clientY / window.innerHeight}deg) rotateY(${30 * clientX / window.innerWidth - 15}deg)`,
     }, { duration: 5000, fill: "forwards" });
 }
 
 
 
 
-const gap = window.innerWidth * 0.08
 scrollContainer.addEventListener("scroll", function () {
-    parallax();
-
-    if (scrollContainer.scrollLeft + scrollContainer.clientWidth >= scrollContainer.scrollWidth - 100) {
-        let children = scrollContainer.children;
-        scrollContainer.appendChild(children[0]);
-        scrollContainer.appendChild(children[1]);
-        scrollContainer.scrollLeft = scrollContainer.scrollLeft - 2 * children[0].offsetWidth - gap;
-    }
-
-    if (scrollContainer.scrollLeft === 0) {
-        let children = scrollContainer.children;
-        scrollContainer.prepend(children[children.length - 1]);
-        scrollContainer.prepend(children[children.length - 2]);
-        scrollContainer.scrollLeft = scrollContainer.scrollLeft + 2 * children[0].offsetWidth;
+    let ticking = false;
+    if (!ticking) {
+        // event throtteling
+        window.requestAnimationFrame(function () {
+            parallax();
+            atScrollEnd();
+            ticking = false;
+        });
+        ticking = true;
     }
 });
 
@@ -149,9 +146,9 @@ function parallax() {
         const divRect = element.getBoundingClientRect();
         if (divRect.left >= 0 && divRect.left <= window.innerWidth || divRect.right >= 0 && divRect.right <= window.innerWidth) {
             element.scrollLeft = divRect.left * 0.06 + 20;
-            element.style.transform = `rotateZ(${divRect.left *20 / window.innerWidth - 12}deg) rotateY(${divRect.left *20 / window.innerWidth - 12}deg)`;
-            element.style.scale = `${1.2 - Math.abs(divRect.left/window.innerWidth - 0.35)}`;
-            element.parentNode.style.transform = `rotateY(${divRect.left *20 / window.innerWidth - 12}deg)`;
+            element.style.transform = `rotateZ(${divRect.left * 20 / window.innerWidth - 12}deg) rotateY(${divRect.left * 20 / window.innerWidth - 12}deg)`;
+            element.style.scale = `${1.2 - Math.abs(divRect.left / window.innerWidth - 0.35)}`;
+            element.parentNode.style.transform = `rotateY(${divRect.left * 20 / window.innerWidth - 12}deg)`;
         }
     });
 }
@@ -177,7 +174,23 @@ function menuItemsAnimeClose() {
         [...menuItem.children].forEach((letter) => {
             letter.animate({
                 opacity: '0'
-            }, { duration: 200, fill: 'forwards'});
+            }, { duration: 200, fill: 'forwards' });
         })
     });
+}
+
+function atScrollEnd() {
+    if (scrollContainer.scrollLeft + scrollContainer.clientWidth >= scrollContainer.scrollWidth - 100) {
+        let children = scrollContainer.children;
+        scrollContainer.appendChild(children[0]);
+        scrollContainer.appendChild(children[1]);
+        scrollContainer.scrollLeft = scrollContainer.scrollLeft - 2 * children[0].offsetWidth - gap;
+    }
+
+    if (scrollContainer.scrollLeft === 0) {
+        let children = scrollContainer.children;
+        scrollContainer.prepend(children[children.length - 1]);
+        scrollContainer.prepend(children[children.length - 2]);
+        scrollContainer.scrollLeft = scrollContainer.scrollLeft + 2 * children[0].offsetWidth;
+    }
 }
